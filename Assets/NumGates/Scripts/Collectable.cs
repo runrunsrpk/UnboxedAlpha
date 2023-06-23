78d6d4e521a65bfaf26ca190843cb253ac8d8771
srpk <saranpongk@gmail.com>
2023-06-23 18:01:33 +0700

    Edit square prefabs

---
+++ Assets/NumGates/Scripts/Collectable.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NumGates
+{
+    public class Collectable : MonoBehaviour
+    {
+        protected virtual void Start()
+        {
+
+        }
+
+        protected virtual void OnCollect()
+        {
+
+        }
+    }
+}
+
